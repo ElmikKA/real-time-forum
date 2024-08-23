426022a3a56cd111083c4db3d74c398f577ba7c5
elmik <elmik.karl@gmail.com>
2024-08-23 16:05:48 +0300

    Added fetch functions

--- frontend/static/services/api.js
+++ frontend/static/services/api.js
@@ -1,3 +1,85 @@
+import { hideLoginSection, clearLoginForm, showLoginSection, hideRegistrationSection } from "./auth";
+
+export async function loginFetch(loginCredentials) {
+    try {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginCredentials)
+        });
+
+        if(!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.json();
+        console.log(data)
+        
+        if(data.login !== 'success') {
+            message.innerHTML = data.message;
+        } else {
+            localStorage.setItem('loggedInUser', JSON.stringify(data.user));
+            hideLoginSection();
+            clearLoginForm();
+        }
+
+    } catch (error) {
+        console.log('There was a problem with the login in:', error);
+        message.innerHTML = "An error occured durning log in. Please try again."
+    }
+}
+
+export async function registerFetch (registerForm) {
+    try {
+        const response = fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(registerForm),
+        });
+
+        if(!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        
+        if(data.register !== 'success') {
+            messageDiv.innerHTML = data.message;
+            console.log(data)
+        } else {
+            messageDiv.innerHTML = "Registration successful!";
+            hideRegistrationSection();
+        }
+        console.log(data)
+    } catch(error) {
+        console.log('There was a problem with the registration:', error);
+        messageDiv.innerHTML = "An error occured durning registration. Please try again."
+    }
+}
+
+export async function logoutFetch() {
+    try {
+        const response = await fetch('/api/logout', {
+            method: 'POST',
+        }); 
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        console.log('Logout successful:', response);
+
+        //Cleares user session data
+        localStorage.removeItem('loggedInUser');
+        showLoginSection()
+            
+    } catch(error) {
+        console.error('There was a problem with the logout:', error);
+    }
+}
+
 // Fetching post information
 export async function fetchPosts() {
     try {
@@ -67,6 +149,32 @@ export async function changeLike(type, post_id, comment_id, like) {
 
     } catch (error) {
         console.error('There was a problem liking the post:', error);
+    }
+}
+
+export async function deletePostFetch(id) {
+    const postId = {
+        id: id
+    }
+    
+    try {
+        const response = await fetch("/api/deletePost", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postId)
+        })
+        
+        if(!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        
+        
+        console.log("Post deleted")
+        
+    } catch(error) {
+        console.error('There was a problem deleting the post:', error);
     }
 }
 
